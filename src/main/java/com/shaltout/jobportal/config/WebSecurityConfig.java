785d91c2d3f1389edc3d6bcfcd968572d7b155dd
Gamal-Cs <asd.7shaltwatz@gmail.com>
2026-09-19 22:42:57 +0300

    feat: add Custom Authentication Success Handler

--- src/main/java/com/shaltout/jobportal/config/WebSecurityConfig.java
+++ src/main/java/com/shaltout/jobportal/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,18 +16,22 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class WebSecurityConfig {
     private final CustomUserDetailsService customUserDetailsService;
-    private final String[] publicUrl={
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+    private final String[] publicUrl = {
+            "/",
             "/global-search/**",
             "/register",
             "/register/**",
+
             "/webjars/**",
             "/resources/**",
             "/assets/**",
+            "/css/**",
             "/js/**",
-            "/*.css",
-            "/*.js",
-            "/*.js.map",
-            "/fonts**","/favicon.ico","/resources/**","/error"
+            "/fonts/**",
+
+            "/favicon.ico",
+            "/error"
     };
 
     @Bean
@@ -38,6 +43,13 @@ public class WebSecurityConfig {
                     auth.anyRequest().authenticated();
                 }
         );
+        http.formLogin(form -> form.loginPage("/login").permitAll()
+                .successHandler(customAuthenticationSuccessHandler))
+                .logout(logout -> {
+                    logout.logoutUrl("/logout");
+                    logout.logoutSuccessUrl("/");
+                }).cors(Customizer.withDefaults())
+                .csrf(csrf->csrf.disable());
         return http.build();
     }
 
--- src/main/java/com/shaltout/jobportal/services/UsersService.java
+++ src/main/java/com/shaltout/jobportal/services/UsersService.java
@@ -7,6 +7,7 @@ import com.shaltout.jobportal.repository.JobSeekerProfileRepository;
 import com.shaltout.jobportal.repository.RecruiterProfileRepository;
 import com.shaltout.jobportal.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -19,10 +20,12 @@ public class UsersService {
     private final UsersRepository usersRepository;
     private final JobSeekerProfileRepository jobSeekerProfileRepository;
     private final RecruiterProfileRepository recruiterProfileRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Users addNew(Users users){
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
+        users.setPassword(passwordEncoder.encode(users.getPassword()));
         Users savedUser = usersRepository.save(users);
         if(users.getUserTypeId().getUserTypeId()==1){
             recruiterProfileRepository.save(new RecruiterProfile(savedUser));

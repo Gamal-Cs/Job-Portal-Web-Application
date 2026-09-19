e1f7c0eeacce8cedb2413d1ba704f20f8b3b44f2
Gamal-Cs <asd.7shaltwatz@gmail.com>
2026-09-19 22:08:41 +0300

    develp custom user authentication & authorization

--- src/main/java/com/shaltout/jobportal/config/WebSecurityConfig.java
+++ src/main/java/com/shaltout/jobportal/config/WebSecurityConfig.java
@@ -1,12 +1,20 @@
 package com.shaltout.jobportal.config;
 
+import com.shaltout.jobportal.services.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+    private final CustomUserDetailsService customUserDetailsService;
     private final String[] publicUrl={
             "/global-search/**",
             "/register",
@@ -23,6 +31,7 @@ public class WebSecurityConfig {
 
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authenticationProvider(authenticationProvider());
         http.authorizeHttpRequests(
                 auth -> {
                     auth.requestMatchers(publicUrl).permitAll();
@@ -31,4 +40,19 @@ public class WebSecurityConfig {
         );
         return http.build();
     }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider =
+                new DaoAuthenticationProvider(customUserDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
